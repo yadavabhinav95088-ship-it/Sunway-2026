f84eb5b7b41c715e7ca8f0876d2a315b385a40cc
yadavabhinav95088-ship-it <yadavabhinav95088@gmail.com>
2026-01-10 21:30:37 +0530

    Remove enemy creation and collision logic
    Removed the createEnemy function and its logic.

--- script.js
+++ script.js
@@ -18,43 +18,7 @@ document.addEventListener("keydown", e => {
   player.style.left = lanes[laneIndex] + "px";
 });
 
-// Create enemy
-function createEnemy() {
-  if (gameOver) return;
-
-  let enemy = document.createElement("div");
-  enemy.classList.add("enemy");
-
-  let enemyLane = Math.floor(Math.random() * 3);
-  enemy.style.left = lanes[enemyLane] + "px";
-  enemy.style.top = "-120px";
-
-  game.appendChild(enemy);
-
-  let enemyMove = setInterval(() => {
-    let enemyTop = enemy.offsetTop;
-    enemy.style.top = enemyTop + 5 + "px";
-
-    // Collision
-    if (
-      enemyTop > 350 &&
-      enemy.style.left === player.style.left
-    ) {
-      alert("Game Over! Score: " + score);
-      gameOver = true;
-      clearInterval(enemyMove);
-      location.reload();
-    }
-
-    // Remove enemy
-    if (enemyTop > 500) {
-      enemy.remove();
-      clearInterval(enemyMove);
-      score++;
-      scoreText.innerText = "Score: " + score;
-    }
-  }, 20);
-}
+/
 
 // Enemy generation
 setInterval(createEnemy, 1500);
